29fffe91e6d64cc7bc39dc06e5a6285657ccaddc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-02 07:58:53 +0300

    Added some if-else conditional message to the get_choices function

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ def get_choices():
     player_choice=input("Enter a choice(rock, paper, scissors): ")
     computer_choice=random.choice(options)
     
+    win_message="You win"
+    lose_message="You lose"
+    
     choices = {
         "player": player_choice,
         "computer": computer_choice
@@ -16,7 +19,11 @@ def get_choices():
         "scissors"
     ]
     
-    return choices
+    if(player_choice == computer_choice):
+        return win_message
+    else:
+        return lose_message
+    
 
 
 
