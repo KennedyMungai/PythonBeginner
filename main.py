de01265083589006f334a2791f276959fb745245
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-02 08:03:43 +0300

    Removed the win and loss messages from the function

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ def get_choices():
     player_choice=input("Enter a choice(rock, paper, scissors): ")
     computer_choice=random.choice(options)
     
-    win_message="You win"
-    lose_message="You lose"
-    
     choices = {
         "player": player_choice,
         "computer": computer_choice
@@ -19,11 +16,7 @@ def get_choices():
         "scissors"
     ]
     
-    if(player_choice == computer_choice):
-        return win_message
-    else:
-        return lose_message
-    
+    return "Write the implementation logic"
 
 
 def check_win(player, computer):
